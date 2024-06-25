088f6263324c81c2f8f406ae6e85ad5a92afd6ed
meddinealtin <medine.altinozefe@gmail.com>
2024-06-25 11:51:40 +0300

    SQL

---
+++ 23_05_SQL.sql
@@ -0,0 +1,15 @@
+CREATE TABLE customers1 (
+  customer_id SERIAL PRIMARY KEY,
+  first_name  VARCHAR(50) NOT NULL,
+  last_name   VARCHAR(50) NOT NULL,
+  country     VARCHAR(50),
+  score       INTEGER
+);
+
+-- Insert customers data
+INSERT INTO customers1 (customer_id, first_name, last_name, country, score) VALUES
+(1,'Maria', 'Cramer', 'Germany',350),
+(2,'John','Steel','USA', 900),
+(3,'Georg','Pipps', 'UK', 750),
+(4,'Martin','Müller','Germany', 500),
+(5,'Peter','Franken','USA', NULL);
